e942e06f90a0df84fc5b35ba2d574df5be3f358f
Tom Dalby <thomasjackdalby@gmail.com>
2018-12-08 00:06:22 +0000

    in prog

--- game/map.py
+++ game/map.py
@@ -1,6 +1,9 @@
 from game.tile import Tile
 from game.tile import TILE_TYPES
 import random
+from PIL import Image
+
+CHUNK_SIZE = 20
 
 class Map:
     def __init__(self, width, height):
@@ -14,3 +17,29 @@ class Map:
 
     def getIndex(self, x, y):
         return x + y * self.width
+
+class MapChunk:
+
+    def __init__(self):
+
+
+def loadMapChunks():
+    im = Image.open('map_tiles.bmp') # Can be many different formats.
+    pix = im.load()
+
+    chunks_wide = im.size.width / CHUNK_SIZE
+    chunks_high = im.size.height / CHUNK_SIZE
+
+    for cx in range(0, chunks_wide):
+        for cy in range(0, chunks_wide):
+
+            for x in range(0, CHUNK_SIZE):
+                for y in range(0, CHUNK_SIZE):
+                    v = pix[cx * CHUNK_SIZE + x, cy * CHUNK_SIZE + y]
+                
+
+def getType(rgb):
+    if v is 0:
+        TILE_TYPES[0]
+
+    print pix[x,y]  
